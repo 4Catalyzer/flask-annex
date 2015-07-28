2d7a3493ab565bae650e905430cabf0a88fd1c04
Jimmy Jia <jjia@4catalyzer.com>
2015-07-28 14:43:57 -0400

    Add get and delete functionality

--- flask_annex/file.py
+++ flask_annex/file.py
@@ -1,4 +1,5 @@
 import flask
+import glob
 import os
 import shutil
 
@@ -13,18 +14,49 @@ class FileAnnex(AnnexBase):
     def __init__(self, root_path):
         self._root_path = root_path
 
-    def _get_filename(self, key, make_dirs=False):
-        filename = os.path.join(self._root_path, key)
+    def _get_filename(self, key):
+        return flask.safe_join(self._root_path, key)
 
-        if make_dirs:
-            dir_name = os.path.dirname(filename)
+    def delete(self, key):
+        os.unlink(self._get_filename(key))
+
+        # Clean up empty directories.
+        key_dir_name = os.path.dirname(key)
+        while key_dir_name:
+            dir_name = self._get_filename(key_dir_name)
+            if os.listdir(dir_name):
+                break
+
+            os.rmdir(dir_name)
+            key_dir_name = os.path.dirname(key_dir_name)
+
+    def delete_many(self, keys):
+        for key in keys:
+            self.delete(key)
+
+    def get_file(self, key, out_filename):
+        in_filename = self._get_filename(key)
+        shutil.copyfile(in_filename, out_filename)
+
+    def list_keys(self, prefix):
+        pattern = self._get_filename('{}*'.format(prefix))
+        filenames = glob.glob(pattern)
+
+        return [
+            os.path.relpath(filename, self._root_path)
+            for filename in filenames
+        ]
+
+    def save_file(self, key, in_filename):
+        out_filename = self._get_filename(key)
+
+        # Create directory for file if needed.
+        key_dir_name = os.path.dirname(key)
+        if key_dir_name:
+            dir_name = self._get_filename(key_dir_name)
             if not os.path.exists(dir_name):
                 os.makedirs(dir_name)
 
-        return filename
-
-    def save_file(self, key, in_filename):
-        out_filename = self._get_filename(key, make_dirs=True)
         shutil.copyfile(in_filename, out_filename)
 
     def send_file(self, key, **options):
