45cb037b87fb89ecff618b198aec5590b7472f4f
Jimmy Jia <tesrin@gmail.com>
2016-09-08 15:57:36 -0400

    0.0.10

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ def system(command):
 
 setup(
     name="Flask-Annex",
-    version='0.0.9',
+    version='0.0.10',
     description="Efficient integration of external storage services for Flask",
     url='https://github.com/4Catalyzer/flask-annex',
     author="Jimmy Jia",
